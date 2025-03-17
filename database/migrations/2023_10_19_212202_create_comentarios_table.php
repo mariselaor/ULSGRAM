90d4b9ec7d7b5957cec79e2b01307c18725a6d20
mariselaor <64675015+mariselaor@users.noreply.github.com>
2025-03-16 23:17:37 -0600

    x

--- database/migrations/2023_10_19_212202_create_comentarios_table.php
+++ database/migrations/2023_10_19_212202_create_comentarios_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations
      *
      * @return void
      */
@@ -23,7 +23,7 @@ return new class extends Migration
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations
      *
      * @return void
      */

0bcf36e713ba04ff4333651fe2f2fdb3c8dbaeee
mariselaor <madelinemejia196@gmail.com>
2025-04-08 21:59:07 -0600

    .

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,34 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
+    //
     public function store(Request $request)
     {
-        // Verificar si el archivo es válido
-        if ($request->hasFile('file') && $request->file('file')->isValid()) {
-            $imagen = $request->file('file');
+        //return "Desde imagen controller";
+        $imagen = $request->file('file');
 
-            // Generar un nombre único para la imagen
-            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        //Manejo de la imagen con intervention/image
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
-            // Manejo de la imagen con Intervention Image
-            $imagenServidor = Image::make($imagen);
-            $imagenServidor->fit(1000, 1000);
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
 
-            // Guardar la imagen en el disco público de Laravel
-            $path = Storage::disk('public')->put('images/' . $nombreImagen, $imagenServidor->stream());
+        $imagenPath = public_path('uploads') . "/" . $nombreImagen;
+        $imagenServidor->save($imagenPath);
 
-            // Devolver el nombre de la imagen en formato JSON
-            return response()->json(['imagen' => $nombreImagen]);
-        }
-
-        return response()->json(['error' => 'No image uploaded or invalid'], 400);
+        return response()->json(['imagen' => $nombreImagen ]);
     }
 }
-
